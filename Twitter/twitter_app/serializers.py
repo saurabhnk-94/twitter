614555cb44d21a85d61d431715c6ba749b94a3d0
saurabhnayak <saurabhnk94@gmail.com>
2019-11-11 08:37:41 +0530

    serializers and forms are done

--- Twitter/twitter_app/serializers.py
+++ Twitter/twitter_app/serializers.py
@@ -2,4 +2,13 @@ from rest_framework import serializers
 from .models import *
 
 class UserSerializers(serializers.ModelSerializer):
-    
+    class Meta:
+        model = User
+        fields = ('id', 'user_name','email_id')
+        
+
+class TweetSerializers(serializers.ModelSerializer):
+    user = UserSerializers()
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'tweet_box', 'date_updated')
