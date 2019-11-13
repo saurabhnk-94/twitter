5f5f949d12d913e5abae929cd36a6d743ab1170e
saurabhnayak <saurabhnk94@gmail.com>
2019-11-13 18:11:31 +0530

    GET body content is working fine

---
+++ Twitter/Javascript/home.js
@@ -0,0 +1,31 @@
+// document.getElementById("tweets-1").addEventListener('l')
+window.addEventListener('load', tweetDisplay)
+
+let tweetHttpRequest;
+
+function tweetDisplay() {
+    tweetHttpRequest = new XMLHttpRequest();
+    if (!tweetHttpRequest) {
+        alert("Could not create request!");
+        return false;
+    }
+
+    tweetHttpRequest.onreadystatechange = tweetResponse;
+    tweetHttpRequest.open('GET', 'http://127.0.0.1:8000/tweet');
+    tweetHttpRequest.send();
+}
+
+function tweetResponse() {
+    if (tweetHttpRequest.readyState === XMLHttpRequest.DONE) {
+        if (tweetHttpRequest.status === 200) {
+            // console.log(tweetHttpRequest.responseText);
+            let tweetData = JSON.parse(tweetHttpRequest.responseText);
+            let idtweetData = document.getElementById('tweet-1');
+            for (let i = 0; i < tweetData.length; i++) {
+                let obj = tweetData[i];
+                idtweetData.insertAdjacentHTML('beforeend', '<div id="tweet-structure">' + obj.tweet_box + '</div>')
+                console.log(tweetData)
+            }
+        }
+    }
+}
